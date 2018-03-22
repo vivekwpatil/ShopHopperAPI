bc44b4684fba3d0f74bb1eab8f3cc77346801cc1
D-YD95PD <felix.porres@ing.be>
2018-03-22 02:34:50 +0100

    added get receipts and get receipts by user id

---
+++ src/main/java/be/ing/api/provider/ItemDBService.java
@@ -0,0 +1,7 @@
+package be.ing.api.provider;
+
+/**
+ * Created by D-YD95PD on 22/03/2018.
+ */
+public class ItemDBService {
+}
---
+++ src/main/java/be/ing/api/rest/dto/ItemResponse.java
@@ -0,0 +1,7 @@
+package be.ing.api.rest.dto;
+
+/**
+ * Created by D-YD95PD on 22/03/2018.
+ */
+public class ItemResponse {
+}
